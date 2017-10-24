9ce59ebabc0867c5c11deb8bfdd62cf2aeb66e9e
miraclesumail <857247710@qq.com>
2017-10-24 19:47:01 +0800

    Create eg7.py

---
+++ eg7.py
@@ -0,0 +1,65 @@
+from inspect import signature
+from functools import wraps
+
+def typeassert(*ty_args, **ty_kwargs):
+    def decorate(func):
+        # If in optimized mode, disable type checking
+        if not __debug__:
+            return func
+
+        # Map function argument names to supplied types
+        sig = signature(func)
+        bound_types = sig.bind_partial(*ty_args, **ty_kwargs).arguments
+        print (bound_types)
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            bound_values = sig.bind(*args, **kwargs)
+            # Enforce type assertions across supplied arguments
+            for name, value in bound_values.arguments.items():
+                if name in bound_types:
+                    if not isinstance(value, bound_types[name]):
+                        raise TypeError(
+                            'Argument {} must be {}'.format(name, bound_types[name])
+                            )
+            return func(*args, **kwargs)
+        return wrapper
+    return decorate
+
+@typeassert(int,str,int)
+def spam(x, y, z):
+     print(x, y, z)
+
+spam(1, '2', 3)
+spam(1, 'hello', 3)
+spam(1, 'sss', 8)
+
+from functools import wraps
+
+class A:
+    # Decorator as an instance method
+    def decorator1(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print('Decorator 1')
+            return func(*args, **kwargs)
+        return wrapper
+
+    # Decorator as a class method
+    @classmethod
+    def decorator2(cls, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print('Decorator 2')
+            return func(*args, **kwargs)
+        return wrapper
+        
+    # As an instance method
+a = A()
+@a.decorator1
+def spam():
+    pass
+# As a class method
+@A.decorator2
+def grok():
+    pass    
