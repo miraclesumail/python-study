843edff46154f77f89b41997d49aefb797a60d4c
miraclesumail <857247710@qq.com>
2017-10-24 20:06:46 -0500

    Update eg7.py

--- eg7.py
+++ eg7.py
@@ -1,6 +1,32 @@
 from inspect import signature
 from functools import wraps
 
+def log_getattribute(cls):
+    # Get the original implementation
+    orig_getattribute = cls.__getattribute__
+
+    # Make a new definition
+    def new_getattribute(self, name):
+        print('getting:', name)
+        return orig_getattribute(self, name)
+
+    # Attach to the class and return
+    cls.__getattribute__ = new_getattribute
+    return cls
+
+# Example use
+@log_getattribute
+class A:
+    def __init__(self,x):
+        self.x = x
+    def spam(self):
+        pass
+
+a = A(42)
+a.x
+
+a.spam()
+     
 def typeassert(*ty_args, **ty_kwargs):
     def decorate(func):
         # If in optimized mode, disable type checking
