9ec4b67a1e01a5c5bc83a4c6fd1685b81ffcef23
sumail <sumail.l@lead-surf.com>
2017-10-26 17:41:53 +0800

    dddd

--- eg12.py
+++ eg12.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+from functools import partial
 class Person:
     def __init__(self, name ,age):
         self.name = name
@@ -9,7 +11,7 @@ class Person:
 
     @name.setter
     def name(self, value):
-        print 'name'
+        print ('name')
         if not isinstance(value, str):
             raise TypeError('name must be a string')
         self._name = value
@@ -25,6 +27,58 @@ class Person:
         self._age = value
 
 pp = Person('sss',18)
-print pp.name
-pp.name = 12
-print pp.name
+print (pp.name)
+print (pp._name)
+pp.name = 'kkk'
+print (pp._name)
+
+def typed_property(name, expected_type):
+    storage_name = '_' + name
+
+    @property
+    def prop(self):
+        return getattr(self, storage_name)
+
+    @prop.setter
+    def prop(self, value):
+        print(self)
+        print(value)
+        if not isinstance(value, expected_type):
+            raise TypeError('{} must be a {}'.format(name, expected_type))
+        setattr(self, storage_name, value)
+
+    return prop
+
+class Person:
+    name = typed_property('name', str)
+    age = typed_property('age', int)
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+pp = Person('www',18)
+pp.age = 11
+print(pp._age)
+
+<__main__.Person object at 0x1185f5518>
+www
+<__main__.Person object at 0x1185f5518>
+18
+<__main__.Person object at 0x1185f5518>
+11
+11
+
+from functools import partial
+
+String = partial(typed_property, expected_type=str)
+Integer = partial(typed_property, expected_type=int)
+
+# Example:
+class Person:
+    name = String('name')
+    age = Integer('age')
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
